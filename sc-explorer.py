e0804abf190f86fe4bfee3a97d9aeec1f8836edd
k0dev <108899333+k0dev@users.noreply.github.com>
2023-06-19 22:29:05 +0200

    actually adding songs to a playlist

--- sc-explorer.py
+++ sc-explorer.py
@@ -10,6 +10,8 @@ from pydub.playback import play
 
 with open('client_id', 'r') as f:  # ! Place your client_id in a file named client_id (same directory)
     client_id = f.read().strip()
+with open('oauth', 'r') as f:      # ! Place your oauth code in a file named oauth (same directory)
+    oauth = f.read().strip()
 
 api_url = 'https://api-v2.soundcloud.com'
 search_endpoint = '/search/tracks'
@@ -18,10 +20,33 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
 }
 
-song_window_queue = queue.Queue(maxsize=10)
+authenticated_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+    'Authorization': 'OAuth ' + oauth
+}
 
-def search_by_genre(genre, query="*"):
-    request_url = api_url + search_endpoint + "?q=" + query + "&filter.genre=" + genre + "&sort=popular&client_id=" + client_id + "&locale=en"
+song_window_queue = queue.Queue(maxsize=3)
+
+# TODO: proper debug output
+
+def create_empty_private_playlist(title):
+    url = api_url + '/playlists?client_id=' + client_id
+    body = {'playlist':{"title": title,"sharing":"private","tracks":[],"_resource_type":"playlist"}}
+    r = requests.post(url, headers=authenticated_headers, json=body)
+    assert r.status_code == 201
+    print('[*] Created empty private playlist "' + title + '"')
+    return str(r.json()['id'])
+
+def add_songs_to_playlist(playlist_id, song_list):
+    url = api_url + '/playlists/' + playlist_id + '?client_id='+ client_id
+    body = {"playlist":{"tracks":song_list}}
+    r = requests.put(url, headers=authenticated_headers, json=body)
+    assert r.status_code == 200
+    print('[*] Added ' + str(len(song_list)) + ' songs to the playlist')
+
+# TODO: pagination
+def search_by_genre(genre, query="*"): # TODO: more filtering... what if a song has 0 comments? or has too few likes? we can filter on those!!
+    request_url = api_url + search_endpoint + "?q=" + query + "&filter.genre=" + genre + "&sort=popular&client_id=" + client_id + "&locale=en&limit=200"
     r = requests.get(request_url, headers=headers)
     assert r.status_code == 200
     return r.json()
@@ -86,6 +111,7 @@ def get_most_commented_song_window_position(client_id, track_id, comments_count,
 
 def fill_song_queue_loop(genre):
     candidates = search_by_genre(genre)['collection']
+    print('[*] Exploring ' + str(len(candidates)) + ' songs')
     for song in candidates:
         if not song['commentable']:
             continue
@@ -98,22 +124,45 @@ def fill_song_queue_loop(genre):
         
         window_position = get_most_commented_song_window_position(client_id, song_id, song_comment_count, 200, song_duration, 10, 600)
         song_window = get_song_window(song_url, 10, window_position)
-        song_window_queue.put({'title': song_title, 'window': song_window}, block=True, timeout=200)
-#        print('[*] "' + song_title + '"   inserted into queue')
+        song_window_queue.put({'title': song_title, 'window': song_window, 'id': song_id, 'window_position': window_position}, block=True) # TODO: non blocking so the thread can actually exit 
+        #print('[*] "' + song_title + '"   inserted into queue')
+
+# TODO: maybe the user has more than 50 playlists
+def get_user_owned_playlists():
+    url = api_url + '/me/library/all?client_id=' + client_id + '&limit=50&offset=0'
+    r = requests.get(url, headers=authenticated_headers)
+    assert r.status_code == 200
+    library = r.json()['collection']
+    library = filter(lambda item: item['type'] == 'playlist', library)
+    return [{'playlist_title': item['playlist']['title'], 'playlist_id': str(item['playlist']['id'])} for item in library]
 
 def main():
-    while True:
+    name = input('Playlist name: ')
+    playlist_id = create_empty_private_playlist(name)
+    song_ids = []
+    running = True
+    while running:
         song = song_window_queue.get()
-        print('[+] Playing: "' + song['title'] + '"')
+        print('[+] Playing: "' + song['title'] + '" (starting from ' + str(song['window_position']) + ' seconds)')
         play(song['window'])
-        input("Like?") # TODO: do the actual insertion to a playlist
+        choice = input("Do you like it? [y/n/nq/yq] ")
+        if choice == 'y':
+            song_ids.append(song['id'])
+        elif choice == 'nq':
+            break
+        elif choice == 'yq':
+            song_ids.append(song['id'])
+            break
+        else:
+            continue
+    print('[+] Adding songs to the playlist "' + name + '"')
+    add_songs_to_playlist(playlist_id, song_ids)
 
 if __name__ == "__main__":
-    time.sleep(2)
-    assert song_window_queue is not None
-    threading.Thread(target=fill_song_queue_loop, args=('hiphoprap',)).start()
-    print("Buffering song queue")
-    time.sleep(10)
-    print("Starting program")
+    t = threading.Thread(target=fill_song_queue_loop, args=('deephouse',))
+    t.start()
+    print("[*] Buffering song queue for 5 seconds...")
+    time.sleep(5)
+    print("[*] Starting program")
     main()
-
+    print("Press ctrl+c to force close the queue filling thread (TODO: quit automatically)") # TODO: !!!!
